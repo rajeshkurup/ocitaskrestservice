e5675e8d222e559506a7b597d7fe990d9efef705
Rajesh Raghavakurup <rajeshraghavakurup@Rajeshs-Air.attlocal.net>
2023-01-22 00:59:14 -0800

    change in sors setup

--- src/main/java/org/oci/task/OciTaskRestServiceApplication.java
+++ src/main/java/org/oci/task/OciTaskRestServiceApplication.java
@@ -64,9 +64,9 @@ public class OciTaskRestServiceApplication extends Application<OciTaskRestServic
         final FilterRegistration.Dynamic cors = environment.servlets().addFilter("CORS", CrossOriginFilter.class);
 
         cors.setInitParameter(CrossOriginFilter.ALLOWED_ORIGINS_PARAM, "*");
-        cors.setInitParameter(CrossOriginFilter.ALLOWED_HEADERS_PARAM, "X-Requested-With,Content-Type,Accept,Origin");
-        cors.setInitParameter(CrossOriginFilter.ALLOWED_METHODS_PARAM, "OPTIONS,GET,PUT,POST,DELETE,HEAD,PATCH");
-        
+        cors.setInitParameter(CrossOriginFilter.ALLOWED_HEADERS_PARAM, "<Headers>");
+        cors.setInitParameter(CrossOriginFilter.ALLOWED_METHODS_PARAM, "OPTIONS,GET,PUT,POST,DELETE,HEAD,PATCH,");
+
         cors.addMappingForUrlPatterns(EnumSet.allOf(DispatcherType.class), true, "/*");
     }
 
